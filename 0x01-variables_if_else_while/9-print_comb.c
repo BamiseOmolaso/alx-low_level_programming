4c67721789cfe19d9dbc223a7fff5024a4f598ad
Oluwabamise <davidbams3@gmail.com>
2023-03-07 15:10:02 -0800

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Print all possible combinations of single-digit numbers.
- * Numbers must be separated by commas and space
- * Numbers should be printed in ascending order
- * You can only use the putchar function 4 times
- * All code should be in the main function
- * DO not use any varible of type char
+ * Description: Print all possible combinations fo single-digit numbers.
+ * Numbers must be separated by commas and a space.
+ * You can only use `putchar` to print to console
+ * You can only use `putchar` up to four times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0 (Sucess)
  */
 int main(void)
 {
 	int i = 0;
-	
-	while(i < 10)
+
+	while (i < 10)
 	{
 		putchar(i + '0');
 		if (i < 9)
@@ -23,5 +23,6 @@ int main(void)
 		i++;
 	}
 	putchar('\n');
-	return(0);
+
+	return (0);
 }
