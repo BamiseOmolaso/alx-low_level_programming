88064d62ae47fabf6e64dc8a6dda37516b68cc6c
Oluwabamise <davidbams3@gmail.com>
2023-03-08 00:49:07 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p' + 'u' + 't' + 'c' + 'h' + 'a' + 'r');
+	_putchar('\n');
+
+	return (0);
+}
+
