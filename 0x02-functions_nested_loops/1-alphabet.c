07def9da2b5565d46a4653fcf8060a908e12d92f
Oluwabamise <davidbams3@gmail.com>
2023-03-08 01:09:30 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - main block
+ * Description: print the alphabet, in lowercase, followed by a new line
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+	return(0);
+}
