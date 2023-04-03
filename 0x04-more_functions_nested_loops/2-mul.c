433309fa6f054bf7d2aa8b456751f5b46af3d436
Oluwabamise <davidbams3@gmail.com>
2023-04-03 02:35:07 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers.
+ * @a: Parameter 1.
+ * @b: Parameter 2.
+ *
+ * Return: Multiply a and b
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
