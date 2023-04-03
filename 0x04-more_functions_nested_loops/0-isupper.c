e8bd4c8a6e62f344649af3547e83c5849742d372
Oluwabamise <davidbams3@gmail.com>
2023-04-03 02:03:24 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,7 @@
  * @c: parameter to check
  * Description: Check character, Return 1 if uppercase, 0 if otherwise
  */
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c >= "A" and c <= "Z")
 		return(1);
