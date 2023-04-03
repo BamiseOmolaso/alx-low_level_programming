97a0d35d8bb53da4ff1514a6f8d70364721f5bfc
Oluwabamise <davidbams3@gmail.com>
2023-04-03 06:59:33 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 10x.
+ * 
+ * Return: void.
+ */
+void more_numbers(void)
+{
+	int i, c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c < 15; c++)
+		{
+			if (c >= 10)
+				_putcar((c / 10) + 48);
+			_putcar((c % 10) + 48);
+		}
+		_putcar('\n');
+	}
+}
+
